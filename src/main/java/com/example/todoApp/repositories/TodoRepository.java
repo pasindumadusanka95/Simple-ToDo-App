da9cb2125ba7e67f2ad4d0f6aa67cea16f7cd472
Pasindu Madusanka <pasindusenerath@gmail.com>
2020-02-08 11:28:09 +0530

    added model and repository

---
+++ src/main/java/com/example/todoApp/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.example.todoApp.repositories;
+
+import com.example.todoApp.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
